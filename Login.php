c0e175da46a391e0b426c127ab8873ad67bedd86
hnraz <84960373+hnraz@users.noreply.github.com>
2021-05-28 15:37:16 +0545

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Safal Drug | Log In Page</title>
+<link rel="icon" href="LOGO.jpg">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="Login.css" />
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="Gudiya">
+<div id="box1">
+
+<div id="Gudiya"> <h2 class="hello"><b><center><br>SAFAL DRUG DRISTRIBUTORS PVT.LTD</center></b></h2></div>
+
+<div id="body"><center><a href="Product.html" target="_self"><input name="BACK" type="submit" value="BACK" id="BACK"/></a></center><br><hr><center><h3><b>User Name : <input type="text" Name="User Name" ><br><br>Password :&nbsp; <input type="password" Name="Password" ></b><br><br><br><a href="quality.html" target="_blank"><input name="Log In" type="submit" value="Log in"/></a></h3><hr>Copyright © 2019</center></div>
+</div>
+</div>
+</body>
+</html>
---
+++ hem.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Products Photos</title>
+<link rel="icon" href="LOGO.jpg" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="Gudiya.css" />
+
+
+</head>
+
+<body>
+<div class="body">
+<div id="body">
+<div id="row">
+<div class="col-md-12" id="GUI"><marquee>{&nbsp;&nbsp;&nbsp;&nbsp;Welcome to Safal Drug Dristributors Pvt.Ltd&nbsp;&nbsp;&nbsp;&nbsp;}</marquee></div>
+</div>
+
+
+
+<div id="row">
+<div class="col-md-12" id="Gudiya"> <h1 class="hello"><b><i><center><u>{ Product Photo's }</u></center></i></b></h1></div>
+</div>
+
+
+
+<div id="row">
+<div class="col-md-12" id="box"></div>
+</div>
+
+
+<div id="row">
+<div class="col-md-12">
+<div class="col-md-3" id="gidi"><a href="index.html" target="_self"><center><h4><b class="re">HOME</b></h4></center></a></div>
+<div class="col-md-3" id="gidi"><a href="Product.html" target="_self"><center><h4><b class="re"><center><h4><b class="re">PRODUCT</b></h4></center></a></div>
+<div class="col-md-3" id="gidi"><a href="Gallery.html" target="_self"><center><h4><b class="re"><center><h4><b class="re">GALLERY</b></h4></center></a></div>
+<div class="col-md-3" id="gidi"><a href="Contact.html" target="_self"><center><h4><b class="re">CONTACT</b></h4></center></a></div>
+
+</div>
+</div>
+
+<div id="row">
+<div class="col-md-12" id="box"></div>
+</div>
+
+<div id="row">
+<div class="col-md-12">
+<div class="col-md-4" id="logo"><hr><div class="gu frame"> <img class="two" src="metoz.jpg"></div><hr><div class="gu frame"> <img class="fore" src="careage.jpg"></div><hr><div class="gu frame"> <img class="two" src="nidglowg.jpg"></div><hr><div class="gu frame"> <img class="two" src="hqngc.jpg"></div><hr></div>
+<div class="col-md-4" id="banner"><hr><div class="gu frame"> <img class="three" src="alita.jpg"></div><hr><div class="gu frame"> <img class="five" src="prostam.jpg"></div><hr><div class="gu frame"> <img class="five" src="nidglowg.jpg"></div><hr><div class="gu frame"> <img class="two" src="shadow.png"><hr></div></div>
+<div class="col-md-4" id="time"><hr><div class="gu frame"> <img class="five" src="itop.jpg"></div><hr><div class="gu frame"> <img class="five" src="candidc.jpg"></div><hr><div class="gu frame"> <img class="two" src="canditral.jpg"><hr><div class="gu frame"> <img class="two" src="livoluck.jpeg"></div><hr></div>
+</div>
+</div>
+		
+
+</div>
+</div>
+</body>
+</html>
